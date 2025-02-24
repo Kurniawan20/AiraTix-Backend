b16a53d58890067699282e8f56a0f86820c89af0
Kurniawan20 <kurniawanrisky551@gmail.com>
2025-02-24 12:29:55 +0700

    commit

--- app/Http/Actions/EventPublic/FilterPage.php
+++ app/Http/Actions/EventPublic/FilterPage.php
@@ -10,7 +10,7 @@ class FilterPage extends Controller
 {
     public function __invoke(Request $request)
     {
-        $event = DB::table('events')
+        $events = DB::table('events')
             ->leftJoin('event_settings', 'events.id', '=', 'event_settings.event_id')
             ->leftJoin('tickets', 'events.id', '=', 'tickets.event_id')
             ->leftJoin('ticket_prices', 'tickets.id', '=', 'ticket_prices.ticket_id')
@@ -24,7 +24,7 @@ class FilterPage extends Controller
                 'events.*',
                 'event_settings.location_details',
                 'event_settings.is_online_event',
-                DB::raw('ticket_prices.price::INTEGER as price'),
+                DB::raw('MIN(ticket_prices.price::INTEGER) as min_price'),
                 DB::raw('(
                     SELECT path
                     FROM images
@@ -39,27 +39,10 @@ class FilterPage extends Controller
                 DB::raw("CONCAT('/events/', events.id) as link")
             )
             ->where('events.status', '=', 'LIVE')
-            // ->whereNull('events.deleted_at')
-            // ->where('events.end_date', '>=', now())
-            ->orderBy('events.start_date', 'asc');
+            ->groupBy('events.id', 'event_settings.location_details', 'event_settings.is_online_event', 'image', 'organizers.name', 'organizers.phone', 'organizers.email')
+            ->orderBy('events.start_date', 'asc')
+            ->paginate(10);
 
-        if (request()->topic_id) {
-            $event = $event->where('events.topic_id', request()->topic_id);
-        }
-
-        if (request()->is_online) {
-            $event = $event->where('event_settings.is_online_event', request()->is_online);
-        }
-
-        if (request()->min_price) {
-            $event = $event->where('ticket_prices.price', '>=', request()->min_price);
-        }
-        if (request()->max_price) {
-            $event = $event->where('ticket_prices.price', '<=', request()->max_price);
-        }
-
-        $event = $event->paginate(10);
-
-        return response()->json($event);
+        return response()->json($events);
     }
 }
