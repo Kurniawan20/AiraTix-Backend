e5942a8dae8bf1acf2ae30eb137d4bf69e03d0d6
Kurniawan20 <kurniawanrisky551@gmail.com>
2025-02-24 12:49:55 +0700

    gsdaf

--- app/Http/Actions/EventPublic/FilterPage.php
+++ app/Http/Actions/EventPublic/FilterPage.php
@@ -10,7 +10,7 @@ class FilterPage extends Controller
 {
     public function __invoke(Request $request)
     {
-        $event = DB::table('events')
+        $events = DB::table('events')
             ->leftJoin('event_settings', 'events.id', '=', 'event_settings.event_id')
             ->leftJoin('tickets', 'events.id', '=', 'tickets.event_id')
             ->leftJoin('ticket_prices', 'tickets.id', '=', 'ticket_prices.ticket_id')
@@ -24,7 +24,7 @@ class FilterPage extends Controller
                 'events.*',
                 'event_settings.location_details',
                 'event_settings.is_online_event',
-                DB::raw('ticket_prices.price::INTEGER as price'),
+                DB::raw('MIN(ticket_prices.price::INTEGER) as price'),
                 DB::raw('(
                     SELECT path
                     FROM images
@@ -39,10 +39,10 @@ class FilterPage extends Controller
                 DB::raw("CONCAT('/events/', events.id) as link")
             )
             ->where('events.status', '=', 'LIVE')
-            ->orderBy('events.start_date', 'asc');
+            ->groupBy('events.id', 'event_settings.location_details', 'event_settings.is_online_event', 'image', 'organizers.name', 'organizers.phone', 'organizers.email')
+            ->orderBy('events.start_date', 'asc')
+            ->paginate(10);
 
-        $event = $event->paginate(10);
-
-        return response()->json($event);
+        return response()->json($events);
     }
 }
